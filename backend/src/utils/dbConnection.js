bf03c405872a1b88d72ffde645469be116d87e95
juan penalver <juanpenalver@juans-Laptop.local>
2024-04-20 15:51:52 +0700

    .env changes

--- backend/src/utils/dbConnection.js
+++ backend/src/utils/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnect = () => {
   mongoose
     .connect(
-      "mongodb+srv://juanpenalversanchez11:N8dy62IYvLSPhDY5@cluster0.981n1wd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      process.env.MONGO_URI
     )
     .then(console.log("DB connected"))
     .catch((err) => console.log(err));
